46a41afc0b2b468cfb1c14d113a18145371af6b7
Mrbysco <Mrbysco@users.noreply.github.com>
2022-09-18 16:51:10 +0200

    Reorder the Bee's AI goals

--- src/main/java/gaia/entity/Bee.java
+++ src/main/java/gaia/entity/Bee.java
@@ -87,12 +87,11 @@ public class Bee extends AbstractGaiaEntity implements IAssistMob, IDayMob, Flyi
 
 	@Override
 	protected void registerGoals() {
-		this.goalSelector.addGoal(0, new FloatGoal(this));
-
 		this.goalSelector.addGoal(3, new RandomStrollGoal(this, 1.0D));
 		this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(ItemTags.FLOWERS), false));
 		this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class, 8.0F));
 		this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(9, new FloatGoal(this));
 
 		this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(Bee.class));
 		this.targetPlayerGoal = new NearestAttackableTargetGoal<>(this, Player.class, true);
