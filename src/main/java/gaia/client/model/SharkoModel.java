40e4dd00dfa41d99b05abd90e5acaf9938721594
Mrbysco <Mrbysco@users.noreply.github.com>
2023-02-28 23:53:09 +0100

    Remove unused code from the Sharko Model

--- src/main/java/gaia/client/model/SharkoModel.java
+++ src/main/java/gaia/client/model/SharkoModel.java
@@ -41,62 +41,6 @@ public class SharkoModel extends EntityModel<Sharko> implements HeadedModel, Arm
 		this.rightleg = this.root.getChild("rightleg");
 	}
 
-//	public static LayerDefinition createBodyLayer() {
-//		MeshDefinition meshdefinition = new MeshDefinition();
-//		PartDefinition partdefinition = meshdefinition.getRoot();
-//
-//		PartDefinition sharko = partdefinition.addOrReplaceChild("sharko", CubeListBuilder.create(), PartPose.offset(0.0F, 23.0F, 0.0F));
-//
-//		PartDefinition waist = sharko.addOrReplaceChild("waist", CubeListBuilder.create().texOffs(32, 35).addBox(-5.0F, 10.0F, 3.0F, 6.0F, 4.0F, 5.0F), PartPose.offset(2.0F, -25.0F, -3.5F));
-//
-//		PartDefinition bodymid = waist.addOrReplaceChild("bodymid", CubeListBuilder.create().texOffs(32, 24).addBox(-4.0F, -5.0F, -5.5F, 8.0F, 5.0F, 6.0F), PartPose.offsetAndRotation(-2.0F, 10.0F, 8.0F, 0.1745F, 0.0F, 0.0F));
-//
-//		PartDefinition bodyback = bodymid.addOrReplaceChild("bodyback", CubeListBuilder.create().texOffs(32, 12).addBox(-7.0F, -8.0F, -4.0F, 14.0F, 8.0F, 4.0F)
-//				.texOffs(32, 0).addBox(-6.0F, -7.0F, -7.0F, 12.0F, 8.0F, 4.0F), PartPose.offsetAndRotation(0.0F, -5.0F, 0.5F, 0.2618F, 0.0F, 0.0F));
-//
-//		PartDefinition rightarm = bodyback.addOrReplaceChild("rightarm", CubeListBuilder.create().texOffs(68, 10).addBox(-3.0F, -2.0F, -1.5F, 3.0F, 8.0F, 3.0F), PartPose.offset(-7.0F, -2.0F, -3.5F));
-//
-//		PartDefinition rightpauldron_r1 = rightarm.addOrReplaceChild("rightpauldron_r1", CubeListBuilder.create().texOffs(68, 0).addBox(-6.0F, -1.0F, -2.5F, 7.0F, 5.0F, 5.0F), PartPose.offsetAndRotation(1.0F, -4.0F, 0.0F, 0.0F, 0.0F, -0.2618F));
-//
-//		PartDefinition rightarmlower = rightarm.addOrReplaceChild("rightarmlower", CubeListBuilder.create().texOffs(68, 33).addBox(-2.0F, 0.0F, -3.5F, 4.0F, 10.0F, 4.0F)
-//				.texOffs(68, 21).addBox(-0.5F, -4.0F, -1.5F, 1.0F, 8.0F, 4.0F), PartPose.offsetAndRotation(-1.5F, 6.0F, 1.5F, -0.6981F, 0.0F, 0.0F));
-//
-//		PartDefinition leftarm = bodyback.addOrReplaceChild("leftarm", CubeListBuilder.create().texOffs(68, 10).mirror().addBox(0.0F, -2.0F, -1.5F, 3.0F, 8.0F, 3.0F).mirror(false), PartPose.offset(7.0F, -2.0F, -3.5F));
-//
-//		PartDefinition leftpauldron_r1 = leftarm.addOrReplaceChild("leftpauldron_r1", CubeListBuilder.create().texOffs(68, 0).mirror().addBox(-1.0F, -1.0F, -2.5F, 7.0F, 5.0F, 5.0F).mirror(false), PartPose.offsetAndRotation(-1.0F, -4.0F, 0.0F, 0.0F, 0.0F, 0.2618F));
-//
-//		PartDefinition leftarmlower = leftarm.addOrReplaceChild("leftarmlower", CubeListBuilder.create().texOffs(68, 33).mirror().addBox(-2.0F, 0.0F, -3.5F, 4.0F, 10.0F, 4.0F).mirror(false)
-//				.texOffs(68, 21).addBox(-0.5F, -4.0F, -1.5F, 1.0F, 8.0F, 4.0F), PartPose.offsetAndRotation(1.5F, 6.0F, 1.5F, -0.6981F, 0.0F, 0.0F));
-//
-//		PartDefinition head = bodyback.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -3.0F, 8.0F, 7.0F, 8.0F)
-//				.texOffs(0, 43).addBox(-3.0F, 0.0F, -6.0F, 6.0F, 4.0F, 5.0F)
-//				.texOffs(0, 15).addBox(0.0F, -9.5F, -3.0F, 0.0F, 8.0F, 10.0F), PartPose.offset(0.0F, -4.5F, -7.0F));
-//
-//		PartDefinition headsnout_r1 = head.addOrReplaceChild("headsnout_r1", CubeListBuilder.create().texOffs(0, 33).addBox(-3.5F, 0.0F, -6.0F, 7.0F, 4.0F, 6.0F), PartPose.offsetAndRotation(0.0F, -4.0F, -3.0F, 0.5236F, 0.0F, 0.0F));
-//
-//		PartDefinition tail1 = waist.addOrReplaceChild("tail1", CubeListBuilder.create().texOffs(32, 44).addBox(-2.0F, 0.0F, -4.0F, 4.0F, 4.0F, 4.0F), PartPose.offsetAndRotation(-2.0F, 11.0F, 8.0F, 0.5236F, 0.0F, 0.0F));
-//
-//		PartDefinition tail2 = tail1.addOrReplaceChild("tail2", CubeListBuilder.create().texOffs(48, 44).addBox(-1.5F, 0.5F, -3.0F, 3.0F, 3.0F, 3.0F), PartPose.offsetAndRotation(0.0F, 3.0F, -0.5F, 0.2618F, 0.0F, 0.0F));
-//
-//		PartDefinition tail3 = tail2.addOrReplaceChild("tail3", CubeListBuilder.create().texOffs(32, 52).addBox(-1.0F, 0.0F, -2.0F, 2.0F, 6.0F, 2.0F), PartPose.offsetAndRotation(0.0F, 3.0F, -0.5F, 0.2618F, 0.0F, 0.0F));
-//
-//		PartDefinition tailend = tail3.addOrReplaceChild("tailend", CubeListBuilder.create().texOffs(40, 52).addBox(-7.0F, 22.0F, -4.5F, 10.0F, 6.0F, 1.0F), PartPose.offset(2.0F, -20.0F, 3.0F));
-//
-//		PartDefinition rightleg = sharko.addOrReplaceChild("rightleg", CubeListBuilder.create().texOffs(100, 10).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 7.0F, 4.0F), PartPose.offsetAndRotation(-3.0F, -13.0F, 2.0F, -0.3491F, 0.0F, 0.0F));
-//
-//		PartDefinition rightleglower = rightleg.addOrReplaceChild("rightleglower", CubeListBuilder.create().texOffs(100, 21).addBox(-1.5F, 0.0F, 0.0F, 3.0F, 3.0F, 3.0F), PartPose.offsetAndRotation(0.0F, 7.0F, -2.0F, 0.7854F, 0.0F, 0.0F));
-//
-//		PartDefinition rightfoot = rightleglower.addOrReplaceChild("rightfoot", CubeListBuilder.create().texOffs(100, 27).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 8.0F, 4.0F), PartPose.offsetAndRotation(0.0F, 1.5F, 3.0F, -0.4363F, 0.0F, 0.0F));
-//
-//		PartDefinition leftleg = sharko.addOrReplaceChild("leftleg", CubeListBuilder.create().texOffs(100, 10).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 7.0F, 4.0F), PartPose.offsetAndRotation(3.0F, -13.0F, 2.0F, -0.3491F, 0.0F, 0.0F));
-//
-//		PartDefinition leftleglower = leftleg.addOrReplaceChild("leftleglower", CubeListBuilder.create().texOffs(100, 21).addBox(-1.5F, 0.0F, 0.0F, 3.0F, 3.0F, 3.0F), PartPose.offsetAndRotation(0.0F, 7.0F, -2.0F, 0.7854F, 0.0F, 0.0F));
-//
-//		PartDefinition leftfoot = leftleglower.addOrReplaceChild("leftfoot", CubeListBuilder.create().texOffs(100, 27).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 8.0F, 4.0F), PartPose.offsetAndRotation(0.0F, 1.5F, 3.0F, -0.4363F, 0.0F, 0.0F));
-//
-//		return LayerDefinition.create(meshdefinition, 128, 64);
-//	}
-
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
