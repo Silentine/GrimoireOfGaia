9435a5e9d09f2f300c0c8d8a509b5f5a2ebb67a9
Mrbysco <Mrbysco@users.noreply.github.com>
2023-02-28 23:55:08 +0100

    Have the Shields extend ShieldItem

--- src/main/java/gaia/item/shield/TieredShieldItem.java
+++ src/main/java/gaia/item/shield/TieredShieldItem.java
@@ -1,22 +1,17 @@
 package gaia.item.shield;
 
-import net.minecraft.tags.ItemTags;
 import net.minecraft.util.LazyLoadedValue;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraftforge.common.ToolAction;
 
 import java.util.function.Supplier;
 
-public class TieredShieldItem extends Item {
+public class TieredShieldItem extends ShieldItem {
 	private final LazyLoadedValue<Ingredient> repairIngredient;
 
 	public TieredShieldItem(Item.Properties properties, Supplier<Ingredient> ingredientSupplier) {
@@ -25,22 +20,8 @@ public class TieredShieldItem extends Item {
 		this.repairIngredient = new LazyLoadedValue<>(ingredientSupplier);
 	}
 
-	public UseAnim getUseAnimation(ItemStack stack) {
-		return UseAnim.BLOCK;
-	}
-
-	public int getUseDuration(ItemStack stack) {
-		return 72000;
-	}
-
-	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-		ItemStack itemstack = player.getItemInHand(hand);
-		player.startUsingItem(hand);
-		return InteractionResultHolder.consume(itemstack);
-	}
-
 	public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
-		return repair.is(ItemTags.PLANKS) || repairIngredient.get().test(repair) || super.isValidRepairItem(toRepair, repair);
+		return repairIngredient.get().test(repair) || super.isValidRepairItem(toRepair, repair);
 	}
 
 	@Override
