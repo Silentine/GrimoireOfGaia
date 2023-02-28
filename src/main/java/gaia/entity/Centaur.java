14afcc215c5326452909b6aa62087c1876e4a9a8
Mrbysco <Mrbysco@users.noreply.github.com>
2023-02-28 20:43:54 +0100

    Update Centaur.java

--- src/main/java/gaia/entity/Centaur.java
+++ src/main/java/gaia/entity/Centaur.java
@@ -73,10 +73,9 @@ public class Centaur extends AbstractAssistGaiaEntity implements RangedAttackMob
 	@Override
 	protected void registerGoals() {
 		this.goalSelector.addGoal(0, new FloatGoal(this));
-
 		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1.0D));
 		this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
-		this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
 		this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(new Class[0]));
 		this.targetPlayerGoal = new NearestAttackableTargetGoal<>(this, Player.class, true);
 		if (GaiaConfig.COMMON.allPassiveMobsHostile.get()) {
