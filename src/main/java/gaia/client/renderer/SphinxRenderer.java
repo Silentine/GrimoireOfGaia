e59b35d2df973d52c95fba8073ca7a5cd0da11d6
Mrbysco <Mrbysco@users.noreply.github.com>
2023-04-10 15:08:27 +0200

    Fix Sphinx size

--- src/main/java/gaia/client/renderer/SphinxRenderer.java
+++ src/main/java/gaia/client/renderer/SphinxRenderer.java
@@ -1,5 +1,6 @@
 package gaia.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import gaia.GrimoireOfGaia;
 import gaia.client.ClientHandler;
 import gaia.client.model.SphinxModel;
@@ -19,6 +20,11 @@ public class SphinxRenderer extends MobRenderer<Sphinx, SphinxModel> {
 		super(context, new SphinxModel(context.bakeLayer(ClientHandler.SPHINX)), ClientHandler.largeShadow);
 		this.addLayer(new CustomHeadLayer<>(this, context.getModelSet()));
 		this.addLayer(new AuraLayer<>(this, () -> new SphinxModel(context.bakeLayer(ClientHandler.SPHINX))));
+	}
+
+	@Override
+	protected void scale(Sphinx sphinx, PoseStack poseStack, float partialTicks) {
+		poseStack.scale(1.25F, 1.25F, 1.25F);
 	}
 
 	@Override
